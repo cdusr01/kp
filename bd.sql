e3e4e82ecba56111fd6c47f1f8bfacc51e4c42e4
cdusr01 <newmail1728@gmail.com>
2025-03-19 13:41:19 +0300

    bd.sql

--- bd.sql
+++ bd.sql
@@ -1,15 +1,14 @@
 CREATE TABLE public.clients (
-	client_id int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
+	client_id int4 GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
 	"name" varchar(255) NOT NULL,
 	kind_property varchar(255) NOT NULL,
 	address varchar(255) NOT NULL,
 	phone varchar(20) NOT NULL,
-	contact varchar(255) NOT NULL,
-	CONSTRAINT clients_pkey PRIMARY KEY (client_id)
+	contact varchar(255) NOT NULL
 );
 
-CREATE TABLE information_schema.kind_credit (
-	kind_credit_id int4 NOT NULL,
+CREATE TABLE public.kind_credit (
+	kind_credit_id int4 GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
 	"name" varchar(255) NULL,
 	conditions text NOT NULL,
 	rate numeric NOT NULL,
@@ -17,13 +16,9 @@ CREATE TABLE information_schema.kind_credit (
 );
 
 CREATE TABLE public.credits (
-	credit_id int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
-	kind_credit_id int4 NOT NULL,
-	client_id int4 NOT NULL,
+	credit_id int4 GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+	kind_credit_id int4 NOT NULL REFERENCES clients(client_id),
+	client_id int4 NOT NULL REFERENCES kind_credit(kind_credit_id),
 	summa numeric NOT NULL,
-	"date" date NOT NULL,
-	CONSTRAINT credits_pkey PRIMARY KEY (credit_id)
+	"date" date NOT NULL
 );
-
-ALTER TABLE public.credits ADD CONSTRAINT credits_client_id_fkey FOREIGN KEY (client_id) REFERENCES public.clients(client_id);
-ALTER TABLE public.credits ADD CONSTRAINT credits_kind_credit_id_fkey FOREIGN KEY (kind_credit_id) REFERENCES public.kind_credit(kind_credit_id);
