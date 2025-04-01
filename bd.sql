cc007ba51dc0c2c51da8600f8154abd444ac0bfd
cdusr01 <newmail1728@gmail.com>
2025-04-01 23:03:52 +0300

    bd+report

--- bd.sql
+++ bd.sql
@@ -22,3 +22,19 @@ CREATE TABLE public.credits (
 	summa numeric NOT NULL,
 	"date" date NOT NULL
 );
+
+INSERT INTO public.clients ("name",kind_property,address,phone,contact) VALUES
+	 ('Иванов Иван','Квартира','Москва, ул. Ленина, д. 1','+79161234567','ivanov@example.com'),
+	 ('Сидоров Никита','Телефон','г.Москва, ул. Пушкина, д.17','+79181212222','sidorovn@mail.ru'),
+	 ('Никитин Андрей','Автомобиль','Казань, ул. Ленина, д.3','+79128311311','sidorova11@example.com');
+
+INSERT INTO public.kind_credit (name,conditions,rate,term) VALUES
+	 ('Потребительский','Подтверждение дохода, возраст от 21 года',15.5,36),
+	 ('Автокредит','Первоначальный взнос от 20%, КАСКО обязательно',9.9,60),
+	 ('Рассрочка','Без превоначального взноса',1.0,6);
+
+INSERT INTO public.credits (kind_credit_id,client_id,summa,"date") VALUES
+	 (1,1,150000,'2024-02-08'),
+	 (6,3,200000,'2025-03-15'),
+	 (6,9,10000000,'2025-03-15');
+
